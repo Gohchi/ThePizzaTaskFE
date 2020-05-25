bc9dcea521715f504931fe0a3adb00c0d1ccbcf1
Martín Farias <martin.akg1991@gmail.com>
2020-05-25 17:20:21 -0300

    First screen

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
+import './App.css';
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+  
+  render() {
+    return (
+      <div className="main">
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
+              The Pizza Task
+            </Typography>
+            <Tooltip title="Cart" aria-label="cart">
+              <IconButton color="inherit" aria-label="cart">
+                <ShoppingCartIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Orders" aria-label="orders">
+              <IconButton color="inherit" aria-label="orders">
+                <ListAltIcon />
+              </IconButton>
+            </Tooltip>
+          </Toolbar>
+        </AppBar>
+        <div className="box">
+          <Button
+            variant="contained" color="primary" size="large"
+            id="order-now">Order now</Button>
+        </div>
+        <a href="http://www.freepik.com" id="bg-owner-info">Background designed by Freepik</a>
+      </div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 import registerServiceWorker from './registerServiceWorker'
+import App from './App'
+import './index.css'
 
 class Root extends Component {
   render() {
     return (
-      <div>
-        The Pizza Task
-      </div>
+      <App />
     )
   }
 }
